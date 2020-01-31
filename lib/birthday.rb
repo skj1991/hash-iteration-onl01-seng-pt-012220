868ab81ca6632a920529239a54cb150f09fef108
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 02:17:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,5 +11,7 @@ def happy_birthday(birthday_kids)
  end
 
  def age_appropriate_birthday(birthday_kids)
-
+   birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!" unless age >= 12
+    end
  end
